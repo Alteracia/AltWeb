4873fa6110a09ce65abc15591ab8d528beaaa596
Alteracia <48139187+Alteracia@users.noreply.github.com>
2021-08-31 21:52:30 +0300

    transfer

---
+++ Runtime/Scripts/Json.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace Alteracia.Web
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true, Inherited = false)]
+    public class JsonPropertiesAttribute : Attribute
+    {
+        private string _jsonName;
+
+        /// <summary>
+        ///   <para></para>
+        /// </summary>
+        /// <param name="oldName">The name of the field before renaming.</param>
+        public JsonPropertiesAttribute(string jsonName) => this._jsonName = jsonName;
+
+        /// <summary>
+        ///   <para>The name of the field before the rename.</para>
+        /// </summary>
+        public string jsonName => this._jsonName;
+    }
+    
+    public class JsonStringArray { public string[] list; }
+    public class JsonArray<T> { public T[] list; }
+    
+    public static class AltJson
+    {
+        public static string FormatJsonText(this string json, System.Type systemType)
+        {
+            json = json.FixOneFieldJson(systemType);
+            // Two levels TODO more
+            foreach (var fieldInfo in systemType.GetFields())
+            {
+                json = json.FormatJsonText(fieldInfo);
+                Type fType = fieldInfo.FieldType.IsArray ? fieldInfo.FieldType.GetElementType() : fieldInfo.FieldType;
+                
+                if (fType == null) continue;
+                foreach (var fn in fType.GetFields())
+                {
+                    json = json.FormatJsonText(fn);
+                    Type ft = fn.FieldType.IsArray ? fn.FieldType.GetElementType() : fn.FieldType;
+                    json = ft?.GetFields().Aggregate(json, (current, field) => current.FormatJsonText(field));
+                }
+            }
+            // If json is array wrap to JsonArray
+            return json.FormatIfJsonArray().FormatIfArrayInArray();
+        }
+
+        public static string FixOneFieldJson(this string json, System.Type systemType)
+        {
+            if (systemType.GetFields().Length != 1)
+                return json;
+            if (!json.StartsWith("\"") && !json.EndsWith("\""))
+                return json;
+
+            // Get Field
+            var fieldName = (systemType.GetFields())[0].Name;
+            // Return valid json
+            return "{\"" + fieldName + "\":" + json + "}";
+        }
+        
+        public static string FormatJsonText(this string json, System.Reflection.MemberInfo memberInfo)
+        {
+            Alteracia.Web.JsonPropertiesAttribute[] attrs =
+                (Alteracia.Web.JsonPropertiesAttribute[]) memberInfo.GetCustomAttributes
+                    (typeof(Alteracia.Web.JsonPropertiesAttribute), false);
+            foreach (var attr in attrs)
+            {
+                json = Regex.Replace(json, $"\"({attr.jsonName}.*?)\"", "\"" + memberInfo.Name + "\"");
+                //json = json.Replace("\"" + attr.jsonName, "\"" + memberInfo.Name + "\""); // TODO FIX to Regex!
+            }
+
+            return json;
+        }
+
+        public static string FormatIfJsonArray(this string json)
+        {
+            return json.StartsWith("[") ? "{ \"list\": " + json + "}" : json;
+        }
+
+        public static string FormatIfArrayInArray(this string json) // TODO regex
+        {
+            // Line
+            json = Regex.Replace(json, @"[\r*\n*]", "");
+            // Rid of spaces
+            json = Regex.Replace(json, @"\[(\s*?)\[", "[[");
+            json = Regex.Replace(json, @"\](\s*?)\]", "]]");
+            //Debug.Log("result:  " + json);
+            //Regex regex = new Regex(@"\[(\s*\r*\n*)\[(.*?)\](\s*\r*\n*)\]");
+            //string newJson = "";
+            /*
+            foreach (string match in regex.)//regex.Matches(json))
+            {
+                newJson += match;
+                //Debug.Log(match);
+            }*/
+            //Debug.Log("Trim return " + json);
+            json = json.Replace("[[", "{-->");
+            json = json.Replace("]]", "<--}");
+            string[] parts = json.Split(new string[] {"-->"}, StringSplitOptions.None);
+            
+            if (parts.Length == 1) return json;
+            
+            json = "";
+            foreach (var part in parts)
+            {
+                string[] sub = part.Split(new string[] {"<--"}, StringSplitOptions.None);
+                if (sub.Length < 2)
+                {
+                    json += part;
+                    continue;
+                }
+                string newPart = "";
+                string[] arrays = sub[0].Split(']');
+                foreach (var array in arrays)
+                {
+                    string newArray = "";
+                    string[] clean = array.Split('[');
+                    string clear = clean[clean.Length > 1 ? 1 : 0];
+                    clear = clear.Replace(',', ':');
+                    newArray = clean.Length > 1 ? clean[0] + clear : clear;
+                    newPart += newArray;
+                }
+                json += newPart + sub[1];
+            }
+            return json;
+        }
+    }
+}
---
+++ Runtime/Scripts/Requests.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using UnityEngine;
+#if !UNITY_WEBGL
+#endif
+using UnityEngine.Networking;
+
+namespace Alteracia.Web
+{
+    [ DebuggerNonUserCode ]
+    public readonly struct AsyncOperationAwaiter : INotifyCompletion
+    {
+        private readonly AsyncOperation _asyncOperation;
+        public bool IsCompleted => _asyncOperation.isDone;
+
+        public AsyncOperationAwaiter( AsyncOperation asyncOperation ) => _asyncOperation = asyncOperation;
+
+        public void OnCompleted( Action continuation ) => _asyncOperation.completed += _ => continuation();
+
+        public void GetResult() { }
+    }
+
+    [ DebuggerNonUserCode ]
+    public readonly struct UnityWebRequestAwaiter : INotifyCompletion
+    {
+        private readonly UnityWebRequestAsyncOperation _asyncOperation;
+
+        public bool IsCompleted => _asyncOperation.isDone;
+
+        public UnityWebRequestAwaiter( UnityWebRequestAsyncOperation asyncOperation ) => _asyncOperation = asyncOperation;
+
+        public void OnCompleted( Action continuation ) => _asyncOperation.completed += _ => continuation();
+
+        public UnityWebRequest GetResult() => _asyncOperation.webRequest;
+    }
+    
+    public static class Requests
+    {
+        /// <summary>
+        /// Get from server using UnitWebRequest
+        /// </summary>
+        /// <param name="uri">Full uri of request</param>
+        /// <param name="callback">On complete callback</param>
+        /// <param name="header">Headers add to request</param>
+        /// <returns></returns>
+        public static IEnumerator Get(string uri, Action<UnityWebRequest> callback, string[] header = null)
+        {
+#if ALT_LOADING_LOG || UNITY_EDITOR
+             int log = Log.Start($"curl -X GET \"{uri}\"" + (header != null ? $"-H \"{header[0]} {header[1]}\" " : " "));
+#endif
+            using (UnityWebRequest request = UnityWebRequest.Get(uri))
+            {
+                if (header != null) request.SetRequestHeader(header[0], header[1]);
+                
+                // Send the request and wait for a response
+                yield return request.SendWebRequest();
+#if ALT_LOADING_LOG || UNITY_EDITOR
+                if (request.isNetworkError || request.isHttpError)
+                    Log.Finish(log, $"{request.error}: {request.downloadHandler.text}");
+                else
+                    Log.Finish(log, $"SUCCESS: data - {request.downloadHandler.data.Length}, text - {request.downloadHandler.text.Length}");
+#endif
+                callback(request);
+            }
+        }
+        
+        /// <summary>
+        /// Load Image from server using UnityWebRequestTexture
+        /// </summary>
+        /// <param name="uri">Full uri of request</param>
+        /// <param name="callback">On complete callback</param>
+        /// <param name="header">Headers add to request</param>
+        /// <returns></returns>
+        public static IEnumerator Image(string uri, Action<UnityWebRequest> callback, string[] header = null)
+        {
+#if ALT_LOADING_LOG || UNITY_EDITOR
+            int log = Log.Start($"curl -X GET \"{uri}\"" + (header != null ? $"-H \"{header[0]}: {header[1]}\" " : " "));
+#endif
+            using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(uri))
+            {
+                if (header != null) request.SetRequestHeader(header[0], header[1]);
+                yield return request.SendWebRequest();
+#if ALT_LOADING_LOG || UNITY_EDITOR
+                if (request.isNetworkError || request.isHttpError)
+                    Log.Finish(log, $"{request.error}: {request.downloadHandler.text}");
+                else
+                    Log.Finish(log, $"SUCCESS: image - {request.downloadHandler.data.Length}, text - {request.downloadHandler.text.Length}");
+#endif
+                callback(request);
+            }
+        }
+
+        public static async Task<UnityWebRequest> Image(string uri, string[] header = null)
+        {
+#if ALT_LOADING_LOG || UNITY_EDITOR
+            int log = Log.Start($"curl -X GET \"{uri}\"" + (header != null ? $"-H \"{header[0]}: {header[1]}\" " : " "));
+#endif
+            UnityWebRequest request = UnityWebRequestTexture.GetTexture(uri, true);
+
+            if (header != null) request.SetRequestHeader(header[0], header[1]);
+            await request.SendWebRequest();
+#if ALT_LOADING_LOG || UNITY_EDITOR
+            if (request.isNetworkError || request.isHttpError)
+                Log.Finish(log, $"{request.error}: {request.downloadHandler.text}");
+            else
+                Log.Finish(log, $"SUCCESS: image - {request.downloadHandler.data.Length}, text - {request.downloadHandler.text.Length}");
+#endif
+            return request;
+        }
+
+        /// <summary>
+        /// Post to server using UnitWebRequest
+        /// </summary>
+        /// <param name="uri">Full uri of request</param>
+        /// <param name="message">Post message</param>
+        /// <param name="callback">On complete callback</param>
+        /// <param name="header">Headers add to request</param>
+        /// <returns></returns>
+        public static IEnumerator Post(string uri, WWWForm message, Action<UnityWebRequest> callback, string[] header = null)
+        {
+#if ALT_LOADING_LOG || UNITY_EDITOR
+            int log = Log.Start($"curl -X POST \"{uri}\"" + (header != null ? 
+                $"-H \"{header[0]}: {header[1]}\" " : " ") + $"-d \"{message}\"");
+#endif
+
+            using (UnityWebRequest request = UnityWebRequest.Post(uri, message))
+            {
+                if (header != null) request.SetRequestHeader(header[0], header[1]);
+                // Send the request and wait for a response
+                yield return request.SendWebRequest();
+#if ALT_LOADING_LOG || UNITY_EDITOR
+                if (request.isNetworkError || request.isHttpError)
+                    Log.Finish(log, $"{request.error}: {request.downloadHandler.text}");
+                else
+                    Log.Finish(log, $"SUCCESS: data - {request.downloadHandler.data.Length}, text - {request.downloadHandler.text.Length}");
+#endif
+                callback(request);
+            }
+        }
+        
+        /*
+        public static TaskAwaiter GetAwaiter(this AsyncOperation asyncOp)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            asyncOp.completed += obj => { tcs.SetResult(null); };
+            return ((Task)tcs.Task).GetAwaiter();
+        }
+        */
+        public static UnityWebRequestAwaiter GetAwaiter(this UnityWebRequestAsyncOperation asyncOp)
+        {
+            return new UnityWebRequestAwaiter(asyncOp);
+        }
+        
+        // TODO Protect from same requests
+        //static Dictionary<string, Task<UnityWebRequest>>
+
+        public static async Task<UnityWebRequest> Post(string uri, string[] header = null)
+        {
+            UnityWebRequest request = UnityWebRequest.Post(uri, "");
+
+            return await SendWebRequest(uri, header, request);
+        }
+        
+        private static async Task<UnityWebRequest> SendWebRequest(string uri, string[] header, UnityWebRequest request)
+        {
+#if ALT_LOADING_LOG || UNITY_EDITOR
+            int log = Log.Start($"curl -X POST \"{uri}\"" + (header != null ? $"-H \"{header[0]}: {header[1]}\" " : " "));
+#endif
+            if (header != null)
+            {
+                for (int i = 0; i < header.Length / 2; i += 2)
+                {
+                    request.SetRequestHeader(header[i], header[i + 1]);
+                }
+            }
+
+            // Send the request and wait for a response
+            await request.SendWebRequest();
+#if ALT_LOADING_LOG || UNITY_EDITOR
+            if (request.isNetworkError || request.isHttpError)
+                Log.Finish(log, $"{request.error}: {request.downloadHandler.text}");
+            else
+                Log.Finish(log,
+                    $"SUCCESS: data - {request.downloadHandler.data.Length}, text - {request.downloadHandler.text.Length}");
+#endif
+            return request;
+        }
+        
+        public static async Task<UnityWebRequest> Post(string uri, string message, string[] header = null)
+        {
+            UnityWebRequest request = UnityWebRequest.Post(uri, message);
+
+            return await SendWebRequest(request);
+        }
+
+        public static async Task<UnityWebRequest> Post(string uri, WWWForm message, string[] header = null)
+        {
+            UnityWebRequest request = UnityWebRequest.Post(uri, message);
+
+            return await SendWebRequest(request);
+        }
+        
+        private static async Task<UnityWebRequest> SendWebRequest(UnityWebRequest request)
+        {
+            UnityWebRequestAsyncOperation operation = request.SendWebRequest();
+
+            while (!operation.isDone)
+            {
+                await Task.Delay(100);
+            }
+
+            return request;
+        }
+        
+        public static async Task<UnityWebRequest> PostJson(string uri, string json, string[] header = null)
+        {
+#if ALT_LOADING_LOG || UNITY_EDITOR
+            int log = Log.Start($"curl -X POST \"{uri}\"" + (header != null ? 
+                $"-H \"{header[0]}: {header[1]}\" " : " ") + $"-d \"{json}\"");
+#endif
+
+            UnityWebRequest request = UnityWebRequest.Put(uri, json);
+            if (header != null) request.SetRequestHeader(header[0], header[1]);
+            request.method = "POST";
+            await request.SendWebRequest();
+#if ALT_LOADING_LOG || UNITY_EDITOR
+            if (request.isNetworkError || request.isHttpError)
+                Log.Finish(log, $"{request.error}: {request.downloadHandler.text}");
+            else
+                Log.Finish(log, $"SUCCESS: data - {request.downloadHandler.data.Length}, text - {request.downloadHandler.text.Length}");
+#endif
+            return request;
+        }
+
+        
+        /// <summary>
+        /// Post to server using UnitWebRequest
+        /// </summary>
+        /// <param name="uri">Full uri of request</param>
+        /// <param name="data">Post data</param>
+        /// <param name="fileName">Post file name</param>
+        /// <param name="callback">On complete callback</param>
+        /// <param name="headers">Headers add to request</param>
+        /// <returns></returns>
+        public static IEnumerator PostMultipartFormData(string uri, byte[] data, string fileName, Action<UnityWebRequest> callback, string[] headers = null)
+        {
+#if ALT_LOADING_LOG || UNITY_EDITOR
+            int log = Log.Start($"curl -X POST \"{uri}\"" + (headers != null ?
+             $"-H \"{headers[0]}: {headers[1]}\" " : " ") + $"-d \"screenshot.png\"");
+#endif
+            List<IMultipartFormSection> formData = new List<IMultipartFormSection>
+            {
+                new MultipartFormFileSection("data", data, fileName, "multipart/form-data") // contentType: image/png
+            };
+
+            using (UnityWebRequest request = UnityWebRequest.Post(uri, formData))
+            {
+                if (headers != null) request.SetRequestHeader(headers[0], headers[1]);
+                yield return request.SendWebRequest();
+#if ALT_LOADING_LOG || UNITY_EDITOR
+                if (request.isNetworkError || request.isHttpError)
+                    Log.Finish(log, $"{request.error}: {request.downloadHandler.text}");
+                else
+                    Log.Finish(log, $"SUCCESS: data - {request.downloadHandler.data.Length}, text - {request.downloadHandler.text.Length}");
+#endif
+                callback(request);
+            }
+        }
+        
+        /// <summary>
+        /// Get redirected url from server using UnitWebRequest
+        /// </summary>
+        /// <param name="uri">Full uri of request</param>
+        /// <param name="callback">On complete callback</param>
+        /// <param name="header">Headers add to request</param>
+        /// <returns></returns>
+        public static IEnumerator GetRedirectionUrl(string uri, Action<string> callback, string[] header = null)
+        {
+            using (UnityWebRequest request = UnityWebRequest.Get(uri))
+            {
+                
+#if UNITY_WEBGL
+                request.downloadHandler.Dispose(); // don't load 
+#else
+                // Does not work in WebGL -> get url from request
+                request.redirectLimit = 0;
+#endif
+                
+                // Add header
+                if (header != null) request.SetRequestHeader(header[0], header[1]);
+                
+                // Send the request and wait for a response
+                yield return request.SendWebRequest();
+#if UNITY_WEBGL
+                callback(request.url);
+#else
+                callback(request.GetResponseHeader("location"));
+#endif
+            }
+        }
+
+    }
+}
